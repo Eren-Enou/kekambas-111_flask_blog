bdf528d13309e458498a1d040c5444613f93ee20
Brian <bstanton773@gmail.com>
2023-04-06 14:41:04 -0500

    Add HTTPTokenAuth and require token for create post endpoint

--- app/blueprints/api/auth.py
+++ app/blueprints/api/auth.py
@@ -1,7 +1,9 @@
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from datetime import datetime
 from app.models import User
 
 basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
 
 @basic_auth.verify_password
 def verify(username, password):
@@ -12,3 +14,16 @@ def verify(username, password):
 @basic_auth.error_handler
 def handle_error(status):
     return {'error': 'Incorrect Username and/or Password'}, status
+
+
+@token_auth.verify_token
+def verify(token):
+    print(token)
+    user = User.query.filter_by(token=token).first()
+    now = datetime.utcnow()
+    if user is not None and user.token_expiration > now:
+        return user
+
+@token_auth.error_handler
+def handle_error(status):
+    return {'error': 'You must have a valid token to perform this action'}, status
